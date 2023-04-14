259a4221292b09b9f2dfc06a02d1cc08729a104a
Brinyan <98044395+Brinyan@users.noreply.github.com>
2023-04-14 08:55:46 -0500

    Add files via upload
    Desarrollo ejercicio #1

---
+++ Desarrollo_ejercicio_1.py
@@ -0,0 +1,41 @@
+#Briyith Paola Prieto Salazar
+#Prueba para postular a Intern Tech - TUL
+#14/04/2023
+
+def highestValuePalindrome (s,n,k, r=0):
+  s = list (s) #Tomar s como lista
+
+  if len(s) != n :
+    print ("Longitud no válida, n = ", len(s))
+    print ("Intente de nuevo.")
+    return "-1"
+
+  for i in range(n//2):
+    if r<=k: 
+      lCount = n - i - 1    
+      if s[i] != s[lCount]: #Validar igualdad en posiciones de la lista 
+          if s[lCount] > s[i]:
+              s[i] = s[lCount]
+          else:
+              s[i] = s[lCount]
+          r += 1                
+
+  return "".join(s)
+
+
+s = input('Ingresa el número "s": ')
+n = int(input('¿Qué longitud tiene s?: '))
+k = int(input('¿Cuántos cambios deseas que realice?: '))
+
+if n > 100000 or n <= 0 : 
+  print("Valor fuera de rango")
+else:
+  print(n, ' ', k, ' ', s, ' ')
+  print(highestValuePalindrome(s,n,k))
+
+if k < 0 or k > 100000 :
+  print("Valor fuera de rango")
+else:
+  print(n, ' ', k, ' ', s, ' ')
+  print(highestValuePalindrome(s,n,k))
+
---
+++ Prueba.py
@@ -0,0 +1,36 @@
+l, k = input().split(' ')
+l = int(l)
+k = int(k)
+n = list(input())
+midl = int(len(n)/2)
+
+
+diff = 0
+for i in range(0,midl):
+    if (n[i] != n[l-1-i]):
+        diff += 1
+
+if (diff > k):
+    print(-1)
+    exit(0)
+
+more = k - diff
+
+count = 0
+for i in range(0,midl):
+    if (n[i] == n[l-1-i]):
+        if (n[i] != '9' and more >= 2):
+            n[i], n[l-1-i] = '9', '9'
+            more -= 2
+        continue
+    maxn = n[i] if (n[i] > n[l-1-i]) else n[l-1-i]
+    if (maxn != '9' and more >= 1):
+        more -= 1
+        maxn = '9'
+    n[i], n[l-1-i] = maxn, maxn
+    #count += 1
+
+if (more > 0):
+    n[midl] = '9'
+        
+print("".join(n))
